0e54a2e72f5f713ac9ce3499b9c7ec87fc7d9b1c
Leonardo Lima Coelho <dev.leonardolima@gmail.com>
2022-10-18 18:17:54 -0400

    feat: producer and consumer

---
+++ simulator/application/kafka/produce.go
@@ -0,0 +1,23 @@
+package kafka
+
+import (
+	route2 "github.com/codeedu/imersaofsfc2-simulator/application/route"
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"log"
+	"time"
+)
+
+func Produce(msg *ckafka.Message) {
+	producer := kafka.NewKafkaProducer()
+	route := route2.NewRoute()
+	json.Unmarshal(msg.Value, &route)
+	route.LoadPositions()
+	positions, err := route.ExportJsonPositions()
+	if err != nil {
+		log.Println(err.Error())
+	}
+	for _, p := range positions {
+		kafka.Publish(p, os.Getenv("KafkaProduceTopic"), producer)
+		time.Sleep(time.Millisecond * 500)
+	}
+}
--- simulator/main.go
+++ simulator/main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"github.com/codeedu/imersaofsfc2-simulator/infra/kafka"
 	"fmt"
-	route2 "github.com/codeedu/imersaofsfc2-simulator/application/route"
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/joho/godotenv"
+	"log"
 )
 
-func main() {
-	route := route2.Route{
-		ID:				"1",
-		ClientID:	"1",
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
 	}
-	route.LoadPositions()
-	stringjson, _ := route.ExportJsonPositions()
-	fmt.Println(stringjson[0])
+}
+
+func main() {
+/* 	producer := kafka.NewKafkaProducer()
+	kafka.Publish("Ola", "route.new-direction", producer)
+ */
+	msgChan := make(chan *ckafka.Message)
+	consumer := kafka.NewKafkaConsumer(msgChan)
+	go consumer.Consume()
+	for msg := range msgChan {
+		fmt.Println(string(msg.Value))
+	}
+	
 }
